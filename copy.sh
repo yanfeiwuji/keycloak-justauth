b90fc01365347884445c4f552a6245ddedfb85d8
烟霏雾集 <1327800522@qq.com>
2021-01-15 14:35:22 +0800

    fx

--- copy.sh
+++ copy.sh
@@ -2,7 +2,9 @@ mvn clean package
 echo "building --- "
 rm -rf ~/tools/keycloak-jb/keycloak/providers
 mkdir ~/tools/keycloak-jb/keycloak/providers/
-cp ./target/keycloak-justauth2-12.0.1-jar-with-dependencies.jar ~/tools/keycloak-jb/keycloak/providers/
+cp ./target/keycloak-justauth-12.0.1-jar-with-dependencies.jar ~/tools/keycloak-jb/keycloak/providers/
+
+cp ./ui/phone-number-add.ftl ~/tools/keycloak-jb/keycloak/themes/base/login/
 echo "copy success"
 # shellcheck disable=SC2028
 echo "\n"
--- src/main/java/com/yfwj/justauth/T.java
+++
@@ -1,8 +0,0 @@
-package com.yfwj.justauth;
-
-/**
- * @author yanfeiwuji
- * @date 2021/1/12 4:44 下午
- */
-public class T {
-}
