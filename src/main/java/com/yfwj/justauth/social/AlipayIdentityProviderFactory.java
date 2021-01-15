eb339b5748166c24b8b574799630208c99376a83
烟霏雾集 <1327800522@qq.com>
2021-01-15 14:47:20 +0800

    fx

--- src/main/java/com/yfwj/justauth/social/AlipayIdentityProviderFactory.java
+++ src/main/java/com/yfwj/justauth/social/AlipayIdentityProviderFactory.java
@@ -14,6 +14,7 @@ import org.keycloak.models.KeycloakSession;
  * @date 2021/1/10 5:48 下午
  */
 
+
 public class AlipayIdentityProviderFactory extends
   AbstractIdentityProviderFactory<JustIdentityProvider>
   implements SocialIdentityProviderFactory<JustIdentityProvider> {
